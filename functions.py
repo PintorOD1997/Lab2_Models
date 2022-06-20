ea4f4e167796177c97b06c08364c1e704cf3909c
Diego Pintor <dpintor1997@gmail.com>
2022-06-20 18:10:14 -0500

    Updated Visualizations, Functionalized Main code, added most results to Notebook

--- functions.py
+++ functions.py
@@ -11,6 +11,8 @@
 
 import numpy as np
 import pandas as pd
+import pyflakes
+
 
 def OB_metrics(data_ob: dict = None) -> True:
     """
@@ -87,3 +89,214 @@ def OB_metrics(data_ob: dict = None) -> True:
     })
     metrics.index = l_ts
     return metrics, ob_m1, ob_m4# Returns metrics dataframe, median of trades and no. of priceLevels
+
+def Model1_Exp_1(midprice: pd.Series = None) -> True:
+    """
+    APT Experiment 1:
+    APT model evaluation for ALL data in the OrderBooks.
+    Utilizes the midprice as the fair price in order to evaluate the APT hypothesis
+    (The best estimator for future prices is the current price)
+
+    Parameters
+    ----------
+    midprice (Series) : TimeSeries containing the orderbook settled midprice
+    for each timestamp
+
+    Returns
+    -------
+    APT_df : DataFrame containing the results of the APT Model Evaluation 
+
+    """
+    # Martingale count, all scenarios
+
+    e1 = [midprice[i] == midprice[i+1] for i in range(len(midprice)-1)]
+    e2 = [midprice[i] != midprice[i+1] for i in range(len(midprice)-1)]
+
+    APT_dic = {"e1" : {"Amount" : sum(e1), "Ratio" : sum(e1)/len(midprice)}, 
+                "e2" : {"Amount" : sum(e2), "Ratio" : sum(e2)/len(midprice)},
+                "total" : len(midprice)-1 }
+    # Asset Pricing Theory, DataFrame results
+    APT_df = pd.DataFrame(APT_dic).T
+    return APT_df
+
+
+
+### Second Experiment: Martingale evaluation of the Asset Pricing theory using minute segmented data
+
+def Model1_Exp_2(midprice: pd.Series = None) -> True:
+    """
+    APT Second Experiment:
+    Now, segmenting all data by Minute (The input orderbook should contain orderbooks data 
+    by day, hour, and minute), finds the martingale evaluation of the orderbook mid prices 
+    settlements fot each minute.
+
+    Parameters
+    ----------
+    midprice (str) : Parameter Description
+
+    Returns
+    -------
+    APT_minute_df : DataFrame containing the Model 1 Martingale results for each timestamp
+    APT_min_results_df : DataFrame containing the previous results summarized in the mean results
+    for each Ratio.
+
+    """
+    midprice = pd.DataFrame(midprice)
+    # Minute indexed dictionary, index is Hour + Minute, to avoid minute reprisal.
+    dic = {}
+    for index, row in midprice.iterrows():
+        key = str(index.hour) + ":" + str(index.minute)
+        value = row["Mid Price"]
+        try:
+            dic[key].append(value)
+        except KeyError:
+            dic[key] = [value]
+    # Martingale Evaluation of the segmented data
+    APT_exp2 = {}
+    for i in list(dic): # <- iteración sobre índices del diccionario
+        e1 = sum([dic[i][i_t] == dic[i][i_t+1] for i_t in range(len(dic[i])-1)])
+        e2 = sum([dic[i][i_t] != dic[i][i_t+1] for i_t in range(len(dic[i])-1)])
+        APT_exp2[i] = (
+            {"e1" :{"Amount" :  e1, "Ratio" : e1/(len(dic[i])-1)}, 
+            "e2" :{"Amount" :  e2, "Ratio" : e2/(len(dic[i])-1)},
+            "total" : len(dic[i])-1
+            }
+            )
+    
+    # Experiment 2 results, for each minute, as DataFrame
+    APT_minute_df = pd.DataFrame(data = {
+        "e1": [APT_exp2[i]["e1"]["Amount"] for i in list(APT_exp2)],
+        "e2": [APT_exp2[i]["e2"]["Amount"] for i in list(APT_exp2)],
+        "Total": [APT_exp2[i]["total"] for i in list(APT_exp2)],
+        "Ratio e1": [APT_exp2[i]["e1"]["Ratio"] for i in list(APT_exp2)],
+        "Ratio e2": [APT_exp2[i]["e2"]["Ratio"] for i in list(APT_exp2)]
+    }, index = list(dic))
+
+    # Experiment 2 results, Ratio mean and total trades
+    APT_min_results_df = pd.DataFrame(data = {
+        "e1 Ratio Mean" : np.array(APT_minute_df["Ratio e1"]).mean(),
+        "e2 Ratio Mean" : np.array(APT_minute_df["Ratio e2"]).mean(),
+        "Total Trades" : np.array(APT_minute_df["Total"]).sum()
+    },index = range(1))
+    return APT_minute_df,APT_min_results_df
+
+
+
+###################################################################################
+# Weighted Midprice Experiments
+
+
+def Model1_Exp_3(data_ob: dict = None) -> True:
+    """
+    APT model, using Volume-Weighted Mid Prices
+    This function obtains the previous 2 experiments, now using
+    the Volume-Weighted Mid Prices instead of the Mid Prices of the orderbooks.
+
+    Parameters
+    ----------
+    data_ob (dict) : Dictionary containing the Orderbook. It is used in order to 
+    obtain the VWMP and extract other relevant data.
+
+    Returns
+    -------
+    WAPT_df : Experiment 1
+    WAPT_minute_df : Experiment 2, all data
+    WAPT_min_results_df : Experiment 2, results summarization
+    """
+    x,_,_ = OB_metrics(data_ob)
+    Wmidprice = x["Weighted MidPrice (Ask)"]
+    e1 = [Wmidprice[i] == Wmidprice[i+1] for i in range(len(Wmidprice)-1)]
+    e2 = [Wmidprice[i] != Wmidprice[i+1] for i in range(len(Wmidprice)-1)] # puede ser definido por antonomasia
+    Wmetricas = {"e1" : {"Amount" : sum(e1), "Ratio" : sum(e1)/len(Wmidprice)}, 
+                "e2" : {"Amount" : sum(e2), "Ratio" : sum(e2)/len(Wmidprice)},
+                "total" : len(Wmidprice)-1 }
+    WAPT_df = pd.DataFrame(Wmetricas).T
+    Wmidprice = pd.DataFrame(Wmidprice)
+    Wdic = {}
+    for index, row in Wmidprice.iterrows():
+        key = str(index.hour) + ":" + str(index.minute)
+        value = row["Weighted MidPrice (Ask)"]
+        try:
+            Wdic[key].append(value)
+        except KeyError:
+            Wdic[key] = [value]
+    WAPT_exp2 = {}
+    for i in list(Wdic):
+        e1 = sum([Wdic[i][i_t] == Wdic[i][i_t+1] for i_t in range(len(Wdic[i])-1)])
+        e2 = sum([Wdic[i][i_t] != Wdic[i][i_t+1] for i_t in range(len(Wdic[i])-1)])
+        WAPT_exp2[i] = (
+            {"e1" :{"Amount" :  e1, "Ratio" : e1/(len(Wdic[i])-1)}, 
+            "e2" :{"Amount" :  e2, "Ratio" : e2/(len(Wdic[i])-1)},
+            "total" : len(Wdic[i])-1
+            }
+            )
+    WAPT_minute_df = pd.DataFrame(data = {
+        "e1": [WAPT_exp2[i]["e1"]["Amount"] for i in list(WAPT_exp2)],
+        "e2": [WAPT_exp2[i]["e2"]["Amount"] for i in list(WAPT_exp2)],
+        "Total": [WAPT_exp2[i]["total"] for i in list(WAPT_exp2)],
+        "Ratio e1": [WAPT_exp2[i]["e1"]["Ratio"] for i in list(WAPT_exp2)],
+        "Ratio e2": [WAPT_exp2[i]["e2"]["Ratio"] for i in list(WAPT_exp2)]
+    }, index = list(WAPT_exp2))
+    WAPT_min_results_df = pd.DataFrame(data = {
+        "e1 Ratio Mean" : np.array(WAPT_minute_df["Ratio e1"]).mean(),
+        "e2 Ratio Mean" : np.array(WAPT_minute_df["Ratio e2"]).mean(),
+        "Total Trades" : np.array(WAPT_minute_df["Total"]).sum()
+    }, index = range(1))
+    return WAPT_df,WAPT_minute_df,WAPT_min_results_df
+
+
+
+
+def Model2(midprice: pd.DataFrame = None,x: pd.DataFrame = None) -> True:
+    """
+    Model #2: The Roll Model
+    This function finds if the premise of the model can be confirmed usin data:
+    The Optimal Spread of The Orderbooks can be estimated using the autocovariance of 
+    the price differences. 
+    This function, calculates said autocovariance, estimates the optimal spread,
+    and summarizes the results in a DataFrame containing Each midprice, its spread, and the
+    calculated spread (also compares the bid and ask data).
+
+    Parameters
+    ----------
+    midprice (DataFrame) : DataFrame containing the midprice info.
+    x (DataFrame) : Dataframe containing the Spread info.
+
+    Returns
+    -------
+    model2_df : Dataframe containing, for each timestamp, The modeled spread, the actual spread
+    the midprice, the bid, ask, modeled bid and modeled ask.
+    roll_df_stats : 
+    """
+    # C constant
+    # MidPrice Delta
+    
+    dP_t = midprice["Mid Price"].diff(1) # Present price changes
+    dP_t_1 = midprice["Mid Price"].shift(1).diff(1) # 1 instant in time shifted mid prices
+    # We now need to find this prices Covariance
+    cov = pd.DataFrame({
+        "dP_t" : dP_t, "dP_t_1" : dP_t_1
+    }).cov().iloc[1,0]
+    # Constant C is Sqrt(-cov)
+    C = np.sqrt(-cov)
+    spread = 2*C
+    model2_df = pd.DataFrame({
+        "Model Spread" : spread,
+        "Spread" : x["Spread"],
+        "Mid Price" : midprice["Mid Price"],
+        "Bid": midprice["Mid Price"] - x["Spread"],
+        "Ask" : midprice["Mid Price"] + x["Spread"],
+        "Model Bid" : midprice["Mid Price"] - C,
+        "Model Ask" : midprice["Mid Price"] + C        
+    },index = x.index)
+    
+   
+    # Modelling spread as a Random Variable and comparing it to the results obtained in Roll
+    roll_df_stats = pd.DataFrame({
+        "Spread Mean" : model2_df["Spread"].mean(),
+        "Spread Variance" : model2_df["Spread"].var(),
+        "Calculated Spread" : model2_df["Model Spread"][0]
+    },index = range(1))
+    roll_df_stats["Spread Mean Difference"] = roll_df_stats["Spread Mean"] - roll_df_stats["Calculated Spread"]
+    
+    return model2_df,roll_df_stats
--- visualizations.py
+++ visualizations.py
@@ -1,10 +1,108 @@
 
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- project: Lab 2: Models                                                                              -- #
 # -- script: visualizations.py : python script with data visualization functions                         -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
-# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- author: PintorOD1997                                                                                -- #
+# -- license: GNU General Public License v3.0                                                            -- #
+# -- repository: https://github.com/PintorOD1997/Lab2_Models                                             -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+import pandas as pd 
+import plotly.express as px
+
+
+def Model1_Prices_Comparison(data: pd.DataFrame = None):
+    """
+    Model 1 Graphical Hypothesis Evaluation
+    This function displays a graph on which the prices and the future prices are
+    overlapped, in order for us to test the Model 1 hypothesis, which is:
+    "The best estimator for the future price is the current price"
+
+    Parameters
+    ----------
+    data (DataFrame) : 
+
+    Returns
+    -------
+    None
+    
+    """
+
+    midpricesshift = data.iloc[1:]
+    df = pd.DataFrame({
+        "Mid Price" : data,
+        "Mid Price t_1" : midpricesshift
+    })
+    fig = px.line(df)
+    fig.show()
+def Model1_graph_results_t1(data: pd.DataFrame = None) -> True:
+    """
+    Experiment 1 Graphical Results Type 1
+    Returns Bar plot for the number of succesful predictions and unsuccesful 
+    for the first experiment of the model
+
+    Parameters
+    ----------
+    data (DataFrame) : Parameter Description
+
+    Returns
+    -------
+    None
+    """
+    fig = px.bar(data, x="Amount")
+    fig.show()
+    
+
+def Model1_graph_results_t2(data: pd.DataFrame = None) -> True:
+    """
+    Experiment 1 Graphical Results Type 2
+    Returns Histogram
+
+    Parameters
+    ----------
+    data (DataFrame) : Parameter Description
+
+    Returns
+    -------
+    None
+    """
+    fig = px.histogram(data,x="Ratio e1")
+    fig.show()
+
+def Model1_graph_results_t3(data: pd.DataFrame = None) -> True:
+    """
+    Experiment 1 Graphical Results
+    Returns Bar plot
+
+    Parameters
+    ----------
+    data (DataFrame) : Parameter Description
+
+    Returns
+    -------
+    None
+    """
+    fig = px.bar(data.drop(columns=["Total Trades"]))
+    fig.show()
+    
+def Model2_graph_results(data: pd.DataFrame = None) -> True:
+    """
+    Function Description here
+    Plots Line plot comparing actual spread vs calculated spread and histogram showing
+    the distribution of the spread.
+
+    Parameters
+    ----------
+    data (DataFrame) : Parameter Description
+
+    Returns
+    -------
+    None
+
+    """
+    fig1 = px.line(data,y=data.columns[0:2])
+    fig2 = px.histogram(data,x="Spread")
+    fig1.show()
+    fig2.show()
